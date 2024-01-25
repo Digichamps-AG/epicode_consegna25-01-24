67ae39a0f73fe01bc78b64301a143749d7b9b748
Digichamps-AG <152968075+Digichamps-AG@users.noreply.github.com>
2024-01-25 15:38:44 +0100

    Update S6L3ES1.py

--- S6L3ES1.py
+++ S6L3ES1.py
@@ -1,6 +1,14 @@
 import pandas as pd
 
 vini_dataset = pd.read_csv(".\wine.csv")
-#print(vini_dataset)
+print(vini_dataset)
 
+#informazioni riassuntive del dataset
 print(vini_dataset.describe())
+
+#media
+media = vini_dataset.mean(numeric_only = True)
+print(media)
+
+#moda
+moda = vini_dataset.mode(numeric_only=True)
